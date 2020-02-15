15e729671ae4d6d2f4c9c532d8790e0abf7d655e
Eliezertadeu <silva.eliezertadeu@gmail.com>
2020-02-14 22:33:13 -0300

    Initial Commit

---
+++ AppCombustivel/AppCombustivel/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppCombustivel
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Calcular_Preco(object sender, EventArgs e)
+        {
+            Double precoAlcool = Convert.ToDouble(txt_etanol.Text);
+            Double precoGasolina = Convert.ToDouble(txt_gasolina.Text);
+            Double preco_max_alcool = 0;
+
+            preco_max_alcool = precoGasolina * 0.7;
+
+           try
+           {
+                if (precoAlcool > preco_max_alcool)
+                {
+                    txt_resultado.Text = "Compensa a Gasolina";
+                }
+                else
+                {
+                    txt_resultado.Text = "Compensa o Alcool";
+                }
+           }
+            catch(Exception ex)
+           {
+                await DisplayAlert("Ops", ex.Message, "OK");
+           }
+            
+        }
+    }
+}
